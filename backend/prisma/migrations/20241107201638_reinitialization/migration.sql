31a1b7f94540750dc215918dba1b4d8dcaa0f849
Owen Mariani <mariani.owen@gmail.com>
2024-11-07 15:24:27 -0500

    fixed migrate db issue

--- backend/prisma/migrations/20241107201638_reinitialization/migration.sql
+++ backend/prisma/migrations/20241107201638_reinitialization/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Trip` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `macroLocation` VARCHAR(191) NOT NULL,
-    `anonymousVoting` BOOLEAN NULL,
+    `anonymousVoting` BOOLEAN NOT NULL DEFAULT false,
     `joinCode` INTEGER NULL,
 
     UNIQUE INDEX `Trip_id_key`(`id`),
@@ -13,9 +13,11 @@ CREATE TABLE `Trip` (
 -- CreateTable
 CREATE TABLE `MicroLocation` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
     `tripId` INTEGER NOT NULL,
 
     UNIQUE INDEX `MicroLocation_id_key`(`id`),
+    INDEX `MicroLocation_tripId_fkey`(`tripId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,16 +28,8 @@ CREATE TABLE `Traveler` (
     `name` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `Traveler_id_key`(`id`),
+    INDEX `Traveler_tripId_fkey`(`tripId`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `_Likers` (
-    `A` INTEGER NOT NULL,
-    `B` INTEGER NOT NULL,
-
-    UNIQUE INDEX `_Likers_AB_unique`(`A`, `B`),
-    INDEX `_Likers_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -47,6 +41,15 @@ CREATE TABLE `_Dislikers` (
     INDEX `_Dislikers_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `_Likers` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_Likers_AB_unique`(`A`, `B`),
+    INDEX `_Likers_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `MicroLocation` ADD CONSTRAINT `MicroLocation_tripId_fkey` FOREIGN KEY (`tripId`) REFERENCES `Trip`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -54,13 +57,13 @@ ALTER TABLE `MicroLocation` ADD CONSTRAINT `MicroLocation_tripId_fkey` FOREIGN K
 ALTER TABLE `Traveler` ADD CONSTRAINT `Traveler_tripId_fkey` FOREIGN KEY (`tripId`) REFERENCES `Trip`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_Likers` ADD CONSTRAINT `_Likers_A_fkey` FOREIGN KEY (`A`) REFERENCES `MicroLocation`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `_Likers` ADD CONSTRAINT `_Likers_B_fkey` FOREIGN KEY (`B`) REFERENCES `Traveler`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE `_Dislikers` ADD CONSTRAINT `_Dislikers_A_fkey` FOREIGN KEY (`A`) REFERENCES `MicroLocation`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_Dislikers` ADD CONSTRAINT `_Dislikers_B_fkey` FOREIGN KEY (`B`) REFERENCES `Traveler`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_Likers` ADD CONSTRAINT `_Likers_A_fkey` FOREIGN KEY (`A`) REFERENCES `MicroLocation`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_Likers` ADD CONSTRAINT `_Likers_B_fkey` FOREIGN KEY (`B`) REFERENCES `Traveler`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
