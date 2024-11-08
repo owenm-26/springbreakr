f15761ee97c08be5c3161440dcd42deb73c5e37b
Owen Mariani <mariani.owen@gmail.com>
2024-11-08 10:47:49 -0500

    create trip works

--- client/src/app/api/create_trip/route.ts
+++ client/src/app/api/create_trip/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   try {
-    const { macroLocation, microLocations, joinCode } = await request.json(); // Extract prompt from request body
+    const { macroLocation, microLocations, joinCode } = await request.json();
     console.log(macroLocation, microLocations, joinCode);
 
     const newTrip = await prisma.trip.create({
@@ -14,28 +14,22 @@ export async function POST(request: Request) {
         microLocations: {
           create: microLocations.map((location: string) => ({
             name: location,
-            dislikers: [],
-            likers: [],
           })),
         },
+        joinCode: joinCode || null, // Add joinCode if provided
       },
       include: { microLocations: true },
     });
 
-    // edit trip to add join code
-    if (joinCode) {
-      console.log("add join code");
-    }
     return NextResponse.json({
       message: "Trip created successfully",
       trip: newTrip,
     });
   } catch (e) {
+    console.error("Error creating trip:", e); // Log full error details
     return NextResponse.json(
       { message: "Failed to create trip", error: e },
       { status: 500 }
     );
-  } finally {
-    await prisma.$disconnect();
   }
 }
