81a31ae8c47f8c79f4f165ac789b61e4dacb55f5
Owen Mariani <mariani.owen@gmail.com>
2024-11-13 09:36:22 -0500

    before breaking

--- client/src/app/itinerary/page.tsx
+++ client/src/app/itinerary/page.tsx
@@ -2,9 +2,9 @@
 import { Trip } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Col, Row } from "antd";
 
 export default function ItineraryPage() {
-  const [locations, setLocations] = useState<string[]>([]);
   const [trip, setTrip] = useState<Trip>();
 
   const searchParams = useSearchParams();
@@ -31,19 +31,19 @@ export default function ItineraryPage() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-600 flex flex-col items-center p-8 ">
-      <h1 className="text-4xl font-extrabold text-white mb-6 animate-bounce">
-        Your Itinerary
+      <h1 className="text-4xl font-extrabold text-white mb-6 ">
+        {trip?.macroLocation}
       </h1>
+      <StepForwardOutlined />
 
       {trip ? (
-        <div className="mt-8 bg-white rounded-lg shadow-lg p-4">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Details</h2>
-          <p className="text-gray-700">
-            Country:{" "}
-            <span className="font-bold text-blue-600">
-              {trip?.macroLocation}
-            </span>
-          </p>{" "}
+        <div>
+          <Row>
+            <Col span={6}>col-6</Col>
+            <Col span={6}>col-6</Col>
+            <Col span={6}>col-6</Col>
+            <Col span={6}>col-6</Col>
+          </Row>
         </div>
       ) : (
         <p>Loading your perfect trip...</p>
